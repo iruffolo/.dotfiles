cba2ed0edd8c2f32cc9f3c56164c03bf138f368e
Ian Ruffolo <ian.ruffolo@gmail.com>
2024-05-07 20:17:27 -0400

    Removed conflicting hotkeys for c-h c-l

--- nvim/lua/plugins/custom/harpoon.lua
+++ nvim/lua/plugins/custom/harpoon.lua
@@ -9,17 +9,17 @@ return {
       vim.keymap.set('n', '<leader>a', mark.add_file)
       vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
 
-      vim.keymap.set('n', '<C-h>', function()
+      -- vim.keymap.set('n', '<C-h>', function()
+      --   ui.nav_file(1)
+      -- end)
+      vim.keymap.set('n', '<C-t>', function()
         ui.nav_file(1)
       end)
-      vim.keymap.set('n', '<C-t>', function()
+      vim.keymap.set('n', '<C-n>', function()
         ui.nav_file(2)
       end)
-      vim.keymap.set('n', '<C-n>', function()
-        ui.nav_file(3)
-      end)
       vim.keymap.set('n', '<C-s>', function()
-        ui.nav_file(4)
+        ui.nav_file(3)
       end)
     end,
   },
