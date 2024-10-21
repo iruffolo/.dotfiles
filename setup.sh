804714801eb28ab9de263965533d54ea84c6ed49
Ian Ruffolo <ian.ruffolo@gmail.com>
2024-10-21 00:47:32 -0400

    Fix ignore file and removed setup script

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s ~/.config/nvim nvim 
-ln -s ~/.config/waybar waybar
-ln -s ~/.config/wofi wofi 
-ln -s ~/.config/hypr hypr 
-
-ln -s ~/.tmux.conf .tmux.conf 
-ln -s ~/.zshrc .zshrc
