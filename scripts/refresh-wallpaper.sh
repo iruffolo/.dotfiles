0fa17e1f49c75e5e5d2e2b06b0aa25cb25fadd98
Ian Ruffolo <ian.ruffolo@gmail.com>
2026-07-10 17:42:20 -0400

    Change for wallpaper refresh script, minor updates with hyprland

--- scripts/refresh-wallpaper.sh
+++ scripts/refresh-wallpaper.sh
@@ -1,28 +1,17 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
-# Get current wallpaper
-# wp="$(hyprctl hyprpaper listloaded)"
-#
-# path='/home/ian/Pictures/backgrounds/'
-# fn="$(ps aux | grep swaybg | awk -F$path '{print $2}' | awk '{print $1}' | xargs)"
-# wp=${path}${fn}
+wp="$1"
 
-# Can pass wallpaper path as arg from waypaper post_command
-wp=$1
+if [ -z "$wp" ]; then
+    echo "Error: no wallpaper path provided" >&2
+    exit 1
+fi
 
-# Refresh pywal colors
-wal -q -i $wp
+wal -q -i "$wp"
 
-
-# Replace background in hyprpaper and hyprlock with new image (or sway)
+sed -i "s#^\(\s*path = \).*#\1$wp#" ~/.dotfiles/.config/hypr/hyprlock.conf
+sed -i "s#^\(\s*wallpaper = \).*#\1$wp#" ~/.dotfiles/.config/hypr/hyprpaper.conf
 sed -i "s#/home/ian/Pictures/backgrounds/.*#$wp fill#g" ~/.dotfiles/.config/sway/config
 sed -i "s#/home/ian/Pictures/backgrounds/.*#$wp#g" ~/.dotfiles/.config/swaylock/config
 
-sed -i "s#/home/ian/Pictures/backgrounds/.*#$wp#g" ~/.dotfiles/.config/hypr/hyprlock.conf
-sed -i "s#/home/ian/Pictures/backgrounds/.*#$wp#g" ~/.dotfiles/.config/hypr/hyprpaper.conf
-
-# Reloads sway config, waybar etc.
-# swaymsg reload
-#
-# Reload apps using pywal colorschemes
 source ~/.dotfiles/scripts/waybar-startup.sh
